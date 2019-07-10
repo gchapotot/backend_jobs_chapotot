90a88715bfea65e5b128d8aca5e0ce73cdfec416
gchapotot <chapotot.guillaume@gmail.com>
2019-07-10 10:09:25 +0200

    add removeShift method

--- level5/lifen/src/App.js
+++ level5/lifen/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 var React = require('react');
 var createReactClass = require('create-react-class');
 
+// App Class
 var App = createReactClass({
   getInitialState: function () {
     return (
@@ -34,6 +35,12 @@ var App = createReactClass({
     // set the state
     this.setState({ workers: this.state.workers });
   },
+  removeShift: function (shiftKey) {
+    // update the state object
+    delete this.state.shifts[shiftKey];
+    // set the state
+    this.setState({ shifts: this.state.shifts });
+  },
   render: function () {
     return (
       <div class="component-wrapper">
@@ -41,12 +48,14 @@ var App = createReactClass({
         <AddWorkerForm addWorker={this.addWorker} /><br />
         <RemoveWorkerForm removeWorker={this.removeWorker} workers={this.state.workers} /><br />
         <ShiftList shifts={this.state.shifts} /><br />
-        <AddShiftForm addShift={this.addShift} workers={this.state.workers} />
+        <AddShiftForm addShift={this.addShift} workers={this.state.workers} /><br />
+        <RemoveShiftForm removeShift={this.removeShift} shifts={this.state.shifts} />
       </div>
     );
   }
 });
 
+// WorkerList Class --- display workers
 var WorkerList = createReactClass({
   render: function () {
     return (
@@ -64,6 +73,7 @@ var WorkerList = createReactClass({
   }
 });
 
+// ShiftList Class --- display shift with assigned woker
 var ShiftList = createReactClass({
   render: function () {
     return (
@@ -81,16 +91,20 @@ var ShiftList = createReactClass({
   }
 });
 
+// AddWorkerForm Class --- display form in order to add new workers
 var AddWorkerForm = createReactClass({
   createWorker: function (e) {
     e.preventDefault();
-    //get the worker object name from the form
+    //get the worker object name and status from the form
     var worker = this.refs.workerName.value;
     var status = this.refs.workerStatus.value;
     //call the addWorker method of the App component
     //to change the state of the worker list by adding an new item
-    if (worker.length > 0) {
+    if (worker.length > 0 && status.length > 0) {
       this.props.addWorker(worker, status);
+    }
+    else {
+      alert ('Please check your Worker List form');
     }
     //reset the form
     this.refs.workerForm.reset();
@@ -121,17 +135,21 @@ var AddWorkerForm = createReactClass({
   }
 });
 
+// AddShiftForm Class --- display form in order to add new shifts
 var AddShiftForm = createReactClass({
   createShift: function (e) {
     e.preventDefault();
-    //get the worker object name from the form
+    //get the shift object day and the worker object name from the form
     var day = this.refs.shiftDay.value;
     var worker = this.refs.WorkerShift.value;
     var workerName = this.props.workers[worker];
-    //call the addWorker method of the App component
+    //call the addShift method of the App component
     //to change the state of the worker list by adding an new item
-    if (day.length > 0) {
+    if (day.length > 0 && workerName !== undefined) {
       this.props.addShift(day, workerName);
+    }
+    else {
+      alert ('Please check your Shift List form');
     }
     //reset the form
     this.refs.shiftForm.reset();
@@ -147,7 +165,7 @@ var AddShiftForm = createReactClass({
           <label for="selectWorker" id="secondLabel">
             Assign a worker:
                   <select id="selectWorker" className="form-control" ref="WorkerShift">
-              <option value="">Choose a worker</option>
+              <option value="">--Choose a worker--</option>
               {
                 Object.keys(this.props.workers).map(function (key) {
                   return <option value={key}>{this.props.workers[key]}</option>
@@ -164,6 +182,7 @@ var AddShiftForm = createReactClass({
   }
 });
 
+// RemoveWorkerForm Class --- display form in order to remove workers
 var RemoveWorkerForm = createReactClass({
   selectWorkertoRemove: function (e) {
     var worker = e.target.value;
@@ -184,6 +203,38 @@ var RemoveWorkerForm = createReactClass({
               {
                 Object.keys(this.props.workers).map(function (key) {
                   return <option value={key}>{this.props.workers[key]}</option>
+                }.bind(this))
+              }
+            </select>
+          </label>
+        </div>
+      </form>
+    )
+
+  }
+});
+
+// RemoveShiftForm Class --- display form in order to remove shifts
+var RemoveShiftForm = createReactClass({
+  selectShifttoRemove: function (e) {
+    var shift = e.target.value;
+    //get the shift object from the form
+    //call the removeShift method of the App component
+    this.props.removeShift(shift);
+    //reset the form
+    this.refs.removeShiftForm.reset();
+  },
+  render: function () {
+    return (
+      <form className="form-inline" ref="removeShiftForm" onChange={this.selectShifttoRemove}>
+        <div className="form-group">
+          <label for="selectShift">
+            Remove a shift:
+                  <select id="selectShift" className="form-control">
+              <option value="">--Please choose a shift--</option>
+              {
+                Object.keys(this.props.shifts).map(function (key) {
+                  return <option value={key}>{this.props.shifts[key]}</option>
                 }.bind(this))
               }
             </select>
